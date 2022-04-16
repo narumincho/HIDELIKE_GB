b75d815ca2a364b821ff0c0627e6d69d65eba955
narumincho <16481886+narumincho@users.noreply.github.com>
2022-04-16 09:55:55 +0900

    remove main

--- main/main.ts
+++
@@ -1,5 +0,0 @@
-import { Elm } from "./src/Main.elm";
-
-const rootElement = document.createElement("div");
-document.body.appendChild(rootElement);
-const app = Elm.Main.init({ node: rootElement, flags: null });
