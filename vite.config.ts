6be5b679c796a0f648dec1cc9dcedf3214d3313b
narumincho <16481886+narumincho@users.noreply.github.com>
2026-09-17 22:33:39 +0900

    余計な依存を削除

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from "vite";
 import { fresh } from "@fresh/plugin-vite";
-import tailwindcss from "@tailwindcss/vite";
 import { originalAssetPlugin } from "./scripts/originalAssetPlugin.ts";
 
 export default defineConfig({
-  plugins: [originalAssetPlugin(), fresh(), tailwindcss()],
+  plugins: [originalAssetPlugin(), fresh()],
 });
